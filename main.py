a7ed1022a24a8c7f0ebef8a51dab197644d022db
vickychauhan4949 <67697413+vickychauhan4949@users.noreply.github.com>
2026-09-02 23:23:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from flask import Flask
 import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    try:
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        with open(path,'r',encoding='utf-8') as f:
-            return f.read()
-    except Exception as e:
-        return f"Error: {e} - index.html not found"
+    with open('index.html','r',encoding='utf-8') as f:
+        return f.read()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
