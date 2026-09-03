50f7b72ac4349df20c46467d13ad187026fd669f
vickychauhan4949 <67697413+vickychauhan4949@users.noreply.github.com>
2026-09-03 10:17:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from flask import Flask
 import os
-
 app = Flask(__name__)
-
 @app.route('/')
 def home():
-    with open('index.html', 'r', encoding='utf-8') as f:
-        return f.read()
-
+    return open('index.html','r',encoding='utf-8').read()
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT",10000)))
